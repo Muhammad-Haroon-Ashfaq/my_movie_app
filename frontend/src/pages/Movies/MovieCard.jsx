e8ba35d0873528539941c62d06ebeb20dff32fd2
Muhammad Haroon chaudhary <117911242+Muhammad-Haroon-Ashfaq@users.noreply.github.com>
2026-04-06 17:13:29 +0500

    Fixed upload setting

--- frontend/src/pages/Movies/MovieCard.jsx
+++ frontend/src/pages/Movies/MovieCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { BASE_URL } from '../constants';
+import { BASE_URL } from "../../redux/constants";
 
 const MovieCard = ({ movie }) => {
   return (
